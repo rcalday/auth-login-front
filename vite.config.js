ee3b62afd1b9ad811d0b6b68d6bf02cfa9ea2bd5
rcalday <carlosalday61@gmail.com>
2025-05-28 22:49:24 +0200

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/auth-login-front/",
+	// base: "/auth-login-front/",
 	plugins: [vue()],
 	// plugins: [vue(), vueDevTools()],
 	// resolve: {
